370d870de0806a26f108c739947706fbd29fb1bd
Chuyang Chen <chuyangchen2018@outlook.com>
2025-05-10 16:33:22 -0400

    Update `ConfigReader` and `configurable` for config_file
    - Modify `ConfigReader.__call__` to accept `config_file`
    - Adjust `configurable` to pass `config_file` to `config_reader`

--- src/phdkit/configlib/configurable.py
+++ src/phdkit/configlib/configurable.py
@@ -6,31 +6,35 @@ def __split_key(key: str) -> list[str]:
     return key.split(".")
 
 
-def configurable(cls: Type, *, config_reader: ConfigReader) -> Type:
-    settings = {}
-    for name, attribute in cls.__dict__.items():
-        if callable(attribute) and hasattr(attribute, "config_key"):
-            settings[attribute.config_key] = attribute
-    cls.settings = settings
+def configurable(config_reader: ConfigReader):
+    def __configurable(cls: Type) -> Type:
+        settings = {}
+        for name, attribute in cls.__dict__.items():
+            if callable(attribute) and hasattr(attribute, "config_key"):
+                settings[attribute.config_key] = attribute
+        cls.settings = settings
 
-    def load_config(self):
-        def __load_key(key: str, config: dict):
-            current_config = config
-            for key in __split_key(key):
-                current_config = current_config[key]
-            return current_config
+        def load_config(self, config_file: str | None = None):
+            def __load_key(key: str, config: dict):
+                current_config = config
+                for key in __split_key(key):
+                    current_config = current_config[key]
+                return current_config
 
-        config = config_reader()
-        for key, setter in self.settings.items():
-            setter(self, __load_key(key, config))
+            config = config_reader(config_file)
+            for key, setter in self.settings.items():
+                setter(self, __load_key(key, config))
 
-    cls.load_config = load_config
-    return cls
+        cls.load_config = load_config
+        return cls
+    return __configurable
 
 
-def setting(setter: Callable, key: str) -> Callable:
-    def wrapper(instance: Type) -> Type:
-        setter.config_key = key
-        return setter(instance)
+def setting(key: str):
+    def __setting(setter: Callable) -> Callable:
+        def wrapper(instance: Type) -> Type:
+            setter.config_key = key
+            return setter(instance)
 
-    return wrapper
+        return wrapper
+    return __setting
