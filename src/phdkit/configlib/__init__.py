fb6c260d7390b56d568ee4e820cd11ad157ee7a5
Chuyang Chen <chuyangchen2018@outlook.com>
2025-05-10 17:40:37 -0400

    Update docstring in `__init__.py`
    - Expand module description for clarity
    - Add placeholder for example usage

--- src/phdkit/configlib/__init__.py
+++ src/phdkit/configlib/__init__.py
@@ -1,3 +1,16 @@
+"""Utilities for loading configuration files to a Python object.
+
+This module provides a declarative way to load configuration files to a Python object. 
+Via the decorator design pattern, you can define a class with designated config loader
+and setting items. The class will be automatically populated with the values from the
+configuration file.
+
+Example usage:
+```python
+... # TODO: Add example usage
+```
+"""
+
 from .configurable import configurable, setting
 from .tomlreader import TomlReader
 from .configreader import ConfigReader
