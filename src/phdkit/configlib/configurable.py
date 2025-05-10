5220cb7969ffaf7ebf51d9bcc359995eea97f7f4
Chuyang Chen <chuyangchen2018@outlook.com>
2025-05-10 17:15:10 -0400

    Refactor configurable function parameters
    - Update `configurable` to accept `read_env` parameter
    - Modify loading logic to handle environment configurations

--- src/phdkit/configlib/configurable.py
+++ src/phdkit/configlib/configurable.py
@@ -6,7 +6,7 @@ def __split_key(key: str) -> list[str]:
     return key.split(".")
 
 
-def configurable(config_reader: ConfigReader):
+def configurable(read_config: ConfigReader, read_env: ConfigReader | None = None):
     def __configurable(cls: Type) -> Type:
         settings = {}
         for name, attribute in cls.__dict__.items():
@@ -21,9 +21,14 @@ def configurable(config_reader: ConfigReader):
                     current_config = current_config[key]
                 return current_config
 
-            config = config_reader(config_file)
+            config = read_config(config_file)
             for key, setter in self.settings.items():
                 setter(self, __load_key(key, config))
+            if read_env:
+                env_config = read_env(config_file)
+                for key, setter in self.settings.items():
+                    if key in env_config:
+                        setter(self, __load_key(key, env_config))
 
         cls.load_config = load_config
         return cls
