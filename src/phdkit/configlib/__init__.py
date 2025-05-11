730a1562fde661fb57b9b8b75008098fef27c766
Chuyang Chen <chuyangchen2018@outlook.com>
2025-05-11 14:58:38 -0400

    Import `Configurable` in `__init__.py`
    - Added `Configurable` to imports from `.configurable`
    - Updated `__all__` to include `Configurable`

--- src/phdkit/configlib/__init__.py
+++ src/phdkit/configlib/__init__.py
@@ -11,8 +11,8 @@ Example usage:
 ```
 """
 
-from .configurable import configurable, setting
+from .configurable import configurable, setting, Configurable
 from .tomlreader import TomlReader
 from .configreader import ConfigReader
 
-__all__ = ["configurable", "setting", "TomlReader", "ConfigReader"]
+__all__ = ["configurable", "setting", "TomlReader", "ConfigReader", "Configurable"]
