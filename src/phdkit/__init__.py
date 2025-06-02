13dbad207a6e3ebd1df0ad4abe9e814db974827c
Chuyang Chen <chuyangchen2018@outlook.com>
2025-06-01 23:05:17 -0400

    Add `infix` module and update `__init__.py`
    - Import `infix` in `__init__.py`
    - Define `__all__` for public API

---
+++ src/phdkit/__init__.py
@@ -0,0 +1,11 @@
+from . import autoretry, batching, configlib, gplog, log, mapreduce, pbar, infix
+
+__all__ = [
+    "autoretry",
+    "batching",
+    "configlib",
+    "gplog",
+    "log",
+    "mapreduce",
+    "pbar",
+]
