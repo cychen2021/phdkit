72607f0abef0df70f76439231c2be51ab9bba95b
Chuyang Chen <chuyangchen2018@outlook.com>
2025-06-01 23:43:38 -0400

    Implement `infix` operator functionality
    - Define `base_infix`, `rbind`, and `lbind` classes
    - Add `make_infix` function to create infix operators
    - Update `__all__` to include `infix`

---
+++ src/phdkit/infix.py
@@ -0,0 +1,49 @@
+from functools import update_wrapper
+
+__all__ = ["infix"]
+
+class base_infix(object):
+    def __init__(self, function):
+        self._function = function
+        update_wrapper(self, self._function)
+        self.lbind = type(f"_or_lbind", (lbind,), {"__or__": lbind.__call__})
+        self.rbind = type(f"_or_rbind", (rbind,), {"__ror__": rbind.__call__})
+
+    def __call__(self, *args, **kwargs):
+        return self._function(*args, **kwargs)
+
+    def left(self, other):
+        """Returns a partially applied infix operator"""
+        return self.rbind(self._function, other)
+
+    def right(self, other):
+        return self.lbind(self._function, other)
+
+class rbind(object):
+    def __init__(self, function, binded):
+        self._function = function
+        update_wrapper(self, self._function)
+        self.binded = binded
+    def __call__(self, other):
+        return self._function(other, self.binded)
+    def reverse(self, other):
+        return self._function(self.binded, other)
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: Waiting for left side>"
+
+class lbind(object):
+    def __init__(self, function, binded):
+        self._function = function
+        update_wrapper(self, self._function)
+        self.binded = binded
+    def __call__(self, other):
+        return self._function(self.binded, other)
+    def reverse(self, other):
+        return self._function(other, self.binded)
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: Waiting for right side>"
+
+def make_infix():
+    return type(f"_or_infix", (base_infix,), {"__or__": base_infix.left, "__ror__": base_infix.right})
+
+infix = make_infix()
