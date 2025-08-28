2c5d2d8d978c0b344ff0708320cde4ad02cd46d4
Chuyang Chen <chuyangchen2018@outlook.com>
2025-08-28 19:09:26 +0000

    Update `TomlReader` initialization and call method
    - Change `__init__` parameter from `path` to `default_path`
    - Update `__call__` method to accept an optional `path` parameter
    - Adjust file opening logic to use `default_path` if no `path` is provided

--- src/phdkit/configlib/tomlreader.py
+++ src/phdkit/configlib/tomlreader.py
@@ -1,14 +1,13 @@
 # TODO: Add more functionalities
 
 import tomllib
-from typing import Callable, Protocol
 from .configurable import ConfigLoader
 
 
 class TomlReader(ConfigLoader):
-    def __init__(self, path: str):
-        self.path = path
+    def __init__(self, default_path: str):
+        self.default_path = default_path
 
-    def __call__(self) -> dict:
-        with open(self.path, "rb") as f:
+    def __call__(self, path: str | None = None) -> dict:
+        with open(path or self.default_path, "rb") as f:
             return tomllib.load(f)
